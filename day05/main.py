11afcddf9bc2cd4056e81c1f69f556c2d68cabb7
Daniel Ketterer <26778610+dketterer@users.noreply.github.com>
2020-12-05 10:34:03 +0100

    improve day05 python

--- day05/main.py
+++ day05/main.py
@@ -1,15 +1,10 @@
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         lines = f.readlines()
-
-    lines = [line.strip() for line in lines]
-    bpasses = [(line[:7], line[7:]) for line in lines]
-
-    rows = [int(bpass[0].replace('F', '0').replace('B', '1'), 2) for bpass in bpasses]
-    cols = [int(bpass[1].replace('L', '0').replace('R', '1'), 2) for bpass in bpasses]
-    seat_ids = [(row * 8) + col for row, col in zip(rows, cols)]
-    print(f'Part 1: {max(seat_ids)}')
+    seat_ids = [int(line.strip().replace('L', '0').replace('R', '1').replace('F', '0').replace('B', '1'), 2) for line in lines]
     seat_ids.sort()
+    print(f'Part 1: {seat_ids[-1]}')
+
     last = seat_ids.pop(0)
     while seat_ids:
         i = seat_ids.pop(0)
